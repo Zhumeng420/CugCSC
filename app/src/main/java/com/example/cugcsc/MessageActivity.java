c94932e6d2772671dd17eb6b1bdb11743695863d
zhumeng <zhumeng@cug.edu.cn>
2022-12-23 21:36:58 +0800

    天气查询、数独小游戏

--- app/src/main/java/com/example/cugcsc/MessageActivity.java
+++ app/src/main/java/com/example/cugcsc/MessageActivity.java
@@ -3,32 +3,55 @@ package com.example.cugcsc;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MessageActivity extends AppCompatActivity {
+public class MessageActivity extends AppCompatActivity implements  View.OnClickListener{
     private TextView Notice;
+    private CardView Weather;
+    private CardView Shudo;
+    private CardView Paint;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
         /******绑定控件*********/
         Notice=findViewById(R.id.notice);//让通知滚动显示
+        Typeface type = Typeface.createFromAsset(getAssets(),"search.otf" );//设置字体
+        Notice.setTypeface(type);
         Notice.setMovementMethod(LinkMovementMethod.getInstance());
         Notice.setEllipsize(TextUtils.TruncateAt.MARQUEE);
         Notice.setSingleLine(true);
         Notice.setSelected(true);
         Notice.setFocusable(true);
         Notice.setFocusableInTouchMode(true);
+        Weather=findViewById(R.id.weather);
+        Weather.setOnClickListener(this);
+        Shudo=findViewById(R.id.shudu);
+        Shudo.setOnClickListener(this);
+        Paint=findViewById(R.id.paint);
+        Paint.setOnClickListener(this);
+        /********设置透明********/
+        View v = findViewById(R.id.serach_view2);
+        v.getBackground().setAlpha(200);//0~255透明度值 ，0为完全透明，255为不透明
+        v=findViewById(R.id.content_view2);
+        v.getBackground().setAlpha(160);
+        v=findViewById(R.id.content_view3);
+        v.getBackground().setAlpha(160);
         /*******底部导航栏*********/
         BottomNavigationView bottomNavigationView=findViewById(R.id.botton_navigation);//定位底部导航栏
         bottomNavigationView.setSelectedItemId(R.id.message);//默认选择主页
@@ -55,5 +78,29 @@ public class MessageActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.weather:{
+                Intent intent=new Intent(MessageActivity.this,WebShowActivity.class);
+                intent.putExtra("url","https://www.qweather.com/");
+                MessageActivity.this.startActivity(intent);
+                break;
+            }
+            case R.id.shudu:{
+                Intent intent=new Intent(MessageActivity.this,WebShowActivity.class);
+                intent.putExtra("url","https://sudoku-cn.com/");
+                MessageActivity.this.startActivity(intent);
+                break;
+            }
+            case R.id.paint:{
+                Intent intent=new Intent(MessageActivity.this,WebShowActivity.class);
+                intent.putExtra("url","http://yige.baidu.com/");
+                MessageActivity.this.startActivity(intent);
+                break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/cugcsc/WebShowActivity.java
@@ -0,0 +1,37 @@
+package com.example.cugcsc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebShowActivity extends AppCompatActivity {
+    private WebView Show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_show);
+        Show=findViewById(R.id.movie_show);
+        Show.setWebChromeClient(new WebChromeClient());
+        //Show.setWebViewClient(new WebViewClient());
+        /******接受数据*******/
+        Intent i=getIntent();
+        String url=i.getStringExtra("url");
+        Show.loadUrl(url);
+        /******浏览器中允许返回*************/
+        WebSettings webSettings=Show.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+    @Override
+    public void onBackPressed(){
+        if(Show.canGoBack()){
+            Show.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
